ef7bf47a45ac99a3a6fbe88c688cb4855d474593
gusvasquez <gustavo.vasquez@corhuila.edu.co>
2024-02-24 10:04:27 -0500

    rutas basicas configuradas

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -21,7 +21,7 @@ class MenuItem {
     MenuItem(
         title: 'Tarjetas',
         subTitle: 'Un contenedor estilizado',
-        link: '/card',
+        link: '/cards',
         icon: Icons.credit_card),
   ];
 }
