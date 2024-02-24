5b57db7af5b54a4f316a551aca38d36f87f1183c
Gustavo Vasquez <gustavo.vasquez@corhuila.edu.co>
2024-02-24 16:56:48 -0500

    screen creados

--- lib/presentation/screen/screens.dart
+++ lib/presentation/screen/screens.dart
@@ -1,3 +1,11 @@
+export 'package:widgets_app/presentation/screen/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screen/app_tutotial/app_tutorial_screen.dart';
+export 'package:widgets_app/presentation/screen/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screen/progress/progress_screen.dart';
+export 'package:widgets_app/presentation/screen/snackbar/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screen/ui_control/ui_control_screen.dart';
+
 export 'package:widgets_app/presentation/screen/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screen/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screen/home/home_screen.dart';
+
